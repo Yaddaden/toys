8f50bfdbb3ee0834e4d9802ab66dde4f7c884560
John Doe <rabahyad@gmail.com>
2023-04-19 23:33:29 +0200

    Page Connexion et Banner jouet

--- src/pages/Connexion.js
+++ src/pages/Connexion.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import "../style/Connexion.css";
 
 const Connexion = () => {
-  return <div></div>;
+  const [formData, setFormData] = useState({
+    login: "",
+    motDePasse: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Données de connexion :", formData);
+  };
+  return (
+    <>
+      <h1 className="titleConnexion">Connectez-vous au Kids House</h1>
+      <form className="connexion" onSubmit={handleSubmit}>
+        <div className="logpass">
+          <label htmlFor="login">Login: </label>
+
+          <input
+            type="text"
+            id="login"
+            name="login"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="logpass">
+          <label htmlFor="motDePasse">Mot de passe : </label>
+
+          <input
+            type="password"
+            id="motDePasse"
+            name="motDePasse"
+            value={formData.motDePasse}
+            onChange={handleChange}
+          />
+        </div>
+        <button className="insButton" type="submit">
+          Connexion
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default Connexion;
--- src/pages/Inscription.js
+++ src/pages/Inscription.js
@@ -31,78 +31,81 @@ const Inscription = () => {
   }, []);
 
   return (
-    <form className="containerInscription" onSubmit={handleSubmit}>
-      <div className="inscripLabel">
-        <div className="blockA">
-          <label htmlFor="nom">Nom : </label>
+    <>
+      <h1 className="titleIncription">Créerz votre compte</h1>
+      <form className="containerInscription" onSubmit={handleSubmit}>
+        <div className="inscripLabel">
+          <div className="blockA">
+            <label htmlFor="nom">Nom : </label>
 
-          <input
-            type="text"
-            id="nom"
-            name="nom"
-            value={formData.nom}
-            onChange={handleChange}
-          />
+            <input
+              type="text"
+              id="nom"
+              name="nom"
+              value={formData.nom}
+              onChange={handleChange}
+            />
+          </div>
         </div>
-      </div>
-      <div className="inscripLabel">
-        <div className="blockB">
-          <label htmlFor="prenom">Prénom : </label>
+        <div className="inscripLabel">
+          <div className="blockB">
+            <label htmlFor="prenom">Prénom : </label>
 
-          <input
-            type="text"
-            id="prenom"
-            name="prenom"
-            value={formData.prenom}
-            onChange={handleChange}
-          />
+            <input
+              type="text"
+              id="prenom"
+              name="prenom"
+              value={formData.prenom}
+              onChange={handleChange}
+            />
+          </div>
         </div>
-      </div>
-      <div className="inscripLabel">
-        <div className="blockE">
-          <label htmlFor="telephone">Téléphone : </label>
+        <div className="inscripLabel">
+          <div className="blockE">
+            <label htmlFor="telephone">Téléphone : </label>
 
-          <input
-            type="number"
-            id="telephone"
-            name="telephone"
-            value={formData.telephone}
-            onChange={handleChange}
-          />
+            <input
+              type="number"
+              id="telephone"
+              name="telephone"
+              value={formData.telephone}
+              onChange={handleChange}
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="inscripLabel">
-        <div className="blockC">
-          <label htmlFor="email">Email : </label>
+        <div className="inscripLabel">
+          <div className="blockC">
+            <label htmlFor="email">Email : </label>
 
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
         </div>
-      </div>
-      <div className="inscripLabel">
-        <div className="blockD">
-          <label htmlFor="motDePasse">Mot de passe : </label>
+        <div className="inscripLabel">
+          <div className="blockD">
+            <label htmlFor="motDePasse">Mot de passe : </label>
 
-          <input
-            type="password"
-            id="motDePasse"
-            name="motDePasse"
-            value={formData.motDePasse}
-            onChange={handleChange}
-          />
+            <input
+              type="password"
+              id="motDePasse"
+              name="motDePasse"
+              value={formData.motDePasse}
+              onChange={handleChange}
+            />
+          </div>
         </div>
-      </div>
-      <button className="insButton" type="submit">
-        Valider
-      </button>
-      <p>{!form ? "Chargement..." : form}</p>
-    </form>
+        <button className="insButton" type="submit">
+          Valider
+        </button>
+        <p>{!form ? "Chargement..." : form}</p>
+      </form>
+    </>
   );
 };
 
